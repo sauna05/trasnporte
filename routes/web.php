0250a537838ce57c429ab5d79cb142c342dd9a85
Alexander Sauna <alexandersauna05@gmail.com>
2024-10-07 16:36:50 -0500

    Se agregaron nuevas modificaciones en el archivo controladorUser

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 // Ruta principal que gestione el login 
 Route::get('/', function () {
-    return view('cliente.routes-show');
+    return view('');
 });
 
 Route::get('/login', [UserController::class, 'loginForm'])->name('loginForm');
